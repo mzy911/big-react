68ea7bd139d983ea2cdfdb918358e12416fd77f6
孟凡荣 <mfr01606587@alibaba-inc.com>
2024-04-18 18:26:07 +0800

    feat: 学习源码

--- packages/shared/internals.ts
+++ packages/shared/internals.ts
@@ -1,5 +1,8 @@
 import * as React from 'react';
 
+// 内部数据共享层：Reconciler中调用Hooks <===>  内部数据共享  <===> React
+// 1、属性一：currentDispatcher：当前正在执行的线程的 Dispatcher
+// 2、属性二 x f ：用来跟踪当前批的配置
 const internals = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
 
 export default internals;
